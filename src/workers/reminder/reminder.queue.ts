f111f4247073dc8672d147bdd9408c50676115a0
Adi Malka <122941157+adimalka14@users.noreply.github.com>
2025-12-11 16:49:27 +0200

    Delete src/workers/reminder/reminder.queue.ts

--- src/workers/reminder/reminder.queue.ts
+++
@@ -1,9 +0,0 @@
-import { Queue } from 'bullmq';
-import { REDIS_HOST, REDIS_PORT } from '../../shared/config/env.config';
-
-export const reminderQueue = new Queue('reminderQueue', {
-    connection: {
-        host: REDIS_HOST,
-        port: Number(REDIS_PORT),
-    },
-});
